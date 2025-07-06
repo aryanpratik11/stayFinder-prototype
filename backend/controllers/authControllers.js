cbe479f8521b900dd3759b6ba5b4ddb806b862de
Aryan Pratik <132287193+aryanpratik11@users.noreply.github.com>
2025-07-06 22:17:29 +0530

    Update authControllers.js

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -18,7 +18,7 @@ export const register = async (req, res) => {
         let token = await tokenGen(user._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
+            secure: process.env.NODE_ENV === "production",
             sameSite: "none",
             domain: ".onrender.com",
             maxAge: 7 * 24 * 60 * 60 * 1000 //7 days in milliseconds
